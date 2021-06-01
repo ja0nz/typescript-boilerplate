e5e4a26694ad31e71c0eaf3beba1ec0d3771b900
ja0nz <mail@ja.nz>
2021-06-01 19:56:34 +0200

    Changes

--- src/index.t.ts
+++ src/index.t.ts
@@ -1,13 +1,11 @@
-import { test } from "uvu";
-import * as assert from "uvu/assert";
-import { greet } from "./index";
+// import { greet } from "./index";
 
-test("the data is peanut butter", () => {
-  assert.is(1, 1);
-});
+// test("the data is peanut butter", () => {
+//   assert.is(1, 1);
+// });
 
-test("greeting", () => {
-  assert.is(greet("Foo"), "Hello Foo");
-});
+// test("greeting", () => {
+//   assert.is(greet("Foo"), "Hello Foo");
+// });
 
-test.run();
+// test.run();
