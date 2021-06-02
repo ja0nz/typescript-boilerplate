7c3a63562865acde77404cd6c8e63976da1578ca
ja0nz <mail@ja.nz>
2021-06-02 15:44:14 +0200

    Added jq/sponge dependencies for dependency add

--- shell.nix
+++ shell.nix
@@ -7,6 +7,8 @@ mkShell {
    xsel # Command-line program for getting and setting the contents of the X selection
    html-tidy # A HTML validator and `tidier`
    shfmt # A shell parser and formatter
+   jq # A lightweight and flexible command-line JSON processor
    deno # A secure runtime for JavaScript and TypeScript
+   moreutils # For sponge command
   ];
 }
